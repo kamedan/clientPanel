7029f853db8a387e1fce1c887af1e033f20ad688
kamedan <med.lamine.amara@gmail.com>
2017-08-06 10:39:17 +0100

    table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,73 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from '@angular/router';
+
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireAuth} from 'angularfire2/auth';
+
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+import { ClientService} from './services/client.service';
+
+
+
+const appRoutes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+];
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCaWR1xE9WKB4K7C09OXUEV7q-v68f59PE',
+    authDomain: 'clientpanel-dee33.firebaseapp.com',
+    databaseURL: 'https://clientpanel-dee33.firebaseio.com',
+    projectId: 'clientpanel-dee33',
+    storageBucket: 'clientpanel-dee33.appspot.com',
+    messagingSenderId: '191171056523'
+  
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireDatabase,
+    AngularFireAuth,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
